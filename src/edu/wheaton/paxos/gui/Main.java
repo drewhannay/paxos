eb553c50145ef3a06f767f224e4541f2a6ba8118
Drew Hannay <drewhannay@gmail.com>
2012-12-02 11:34:51 -0600

    Removing obsolete test code from Main.java

--- src/edu/wheaton/paxos/gui/Main.java
+++ src/edu/wheaton/paxos/gui/Main.java
@@ -6,18 +6,5 @@ public class Main
     public static void main(String[] args)
     {
     	new PostOfficeGUI();
-//    	PostOffice postOffice = new PostOffice();
-//		postOffice.addParticipant(m_participantIdGenerator++);
-//		postOffice.addParticipant(m_participantIdGenerator++);
-//		postOffice.addParticipant(m_participantIdGenerator++);
-//		postOffice.addEvent(new PaxosEvent(m_time++,
-//				new PaxosMessage(0, 1, Decree.createOpaqueDecree(0, "test"))));
-//		postOffice.addEvent(new PaxosEvent(m_time++, 
-//				new PaxosMessage(1, 2, Decree.createOpaqueDecree(0, "ignore me"))));
-//		postOffice.addEvent(new PaxosEvent(m_time++, 
-//				new PaxosMessage(1, 2, Decree.createOpaqueDecree(1, "test2"))));
     }
-
-//	private static int m_participantIdGenerator = 0;
-//	private static int m_time = 1;
 }
